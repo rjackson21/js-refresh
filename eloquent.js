70a61da8e2c304a55915df020ea40a6294466863
Regina Jackson <rjackson156@gmail.com>
2023-06-02 15:43:03 -0400

    Fizz Buzz

---
+++ eloquent.js
@@ -0,0 +1,15 @@
+let number = 1
+while (number <= 100) {
+    if (number % 3 == 0) {
+        console.log("Fizz")
+    }
+    else if (number % 5 == 0) {
+        console.log("Buzz")
+    }
+    else {
+    console.log(number);
+    }
+
+   number = number + 1;
+}
+
